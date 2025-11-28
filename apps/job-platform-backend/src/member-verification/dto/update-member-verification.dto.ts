8c54ee134f99effed1e3586e658117cc066d7304
MinHeinKoKo <kokominhein96@gmail.com>
2025-11-28 13:15:08 +0630

    feat: member verification dto

--- apps/job-platform-backend/src/member-verification/dto/update-member-verification.dto.ts
+++ apps/job-platform-backend/src/member-verification/dto/update-member-verification.dto.ts
@@ -1,4 +1,6 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateMemberVerificationDto } from './create-member-verification.dto';
 
-export class UpdateMemberVerificationDto extends PartialType(CreateMemberVerificationDto) {}
+export class UpdateMemberVerificationDto extends PartialType(
+  CreateMemberVerificationDto,
+) {}
