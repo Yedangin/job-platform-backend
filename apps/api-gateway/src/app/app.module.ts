9c86dac70e7173d97bef62cede8ff4a750d5d468
khantzawthit <khantzawthit5@gmail.com>
2025-12-08 10:42:30 +0630

    refactor: replace dto with proto

--- apps/api-gateway/src/app/app.module.ts
+++ apps/api-gateway/src/app/app.module.ts
@@ -17,7 +17,6 @@ import { CategoryModule } from './category/category.module';
 import { JobPostModule } from './job-post/job-post.module';
 import { ApplyModule } from './apply/apply.module';
 import { InterviewModule } from './interview/interview.module';
-import { ReviewModule } from './review.module';
 import { ReviewModule } from './review/review.module';
 
 @Module({
--- apps/job-service/src/app/review/review.module.ts
+++ apps/job-service/src/app/review/review.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ReviewService } from './review.service';
 import { ReviewController } from './review.controller';
+import { JobPrismaService, PaginationService } from '@in-job/common';
 
 @Module({
   controllers: [ReviewController],
-  providers: [ReviewService],
+  providers: [ReviewService, PaginationService, JobPrismaService],
 })
 export class ReviewModule {}
