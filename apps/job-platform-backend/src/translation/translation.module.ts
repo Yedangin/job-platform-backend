8ac4356eb0604bb661779ed5d10ffe41d8066337
Devkositt <williamskositt@gmail.com>
2026-04-01 09:01:56 +0000

    fix: wire JobPrismaModule in translation module

--- apps/job-platform-backend/src/translation/translation.module.ts
+++ apps/job-platform-backend/src/translation/translation.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { JobPrismaModule } from 'libs/common/src';
 import { TranslationService } from './translation.service';
 
 @Module({
+  imports: [JobPrismaModule],
   providers: [TranslationService],
   exports: [TranslationService],
 })
